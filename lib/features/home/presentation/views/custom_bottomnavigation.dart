78f15bdd84658ea62eb78d755e744e2618e00daa
Ahm3dmohamed <you@example.com>
2024-10-31 16:46:18 +0300

    add custom bottomnavigationbar

---
+++ lib/features/home/presentation/views/custom_bottomnavigation.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomnavigation extends StatelessWidget {
+  const CustomBottomnavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: BottomNavigationBar(items: []),
+    );
+  }
+}
