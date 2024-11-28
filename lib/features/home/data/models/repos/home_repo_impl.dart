56ac6670fca4a4d94f9672d50697f738df234ff4
Ahm3dmohamed <you@example.com>
2024-11-28 19:54:55 +0200

    create features cubit and features book state

--- lib/features/home/data/models/repos/home_repo_impl.dart
+++ lib/features/home/data/models/repos/home_repo_impl.dart
@@ -5,6 +5,8 @@ import 'package:bookly/features/home/data/models/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
+// MVVM
+
 class HomeRepoImpl extends HomeRepo {
   ApiService apiService;
   HomeRepoImpl(this.apiService);
@@ -14,6 +16,8 @@ class HomeRepoImpl extends HomeRepo {
     try {
       var data = await apiService.get(
           endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+
+      // https://www.googleapis.com/books/v1/volumes?Filtering=free-ebooks&q=programming
       List<BookModel> books = [];
       for (var item in data["items"]) {
         books.add(BookModel.fromJson(item));
@@ -36,7 +40,20 @@ class HomeRepoImpl extends HomeRepo {
     try {
       var data =
           await apiService.get(endPoint: 'volumes?q=subject:programming');
-    } catch (e) {}
-    throw UnimplementedError();
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(ServerFailure(
+        e.toString(),
+      ));
+    }
   }
 }
---
+++ lib/features/home/presentation/manager/featured_books_ cubit/features_book_state.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+class FeaturesBookState extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class FeaturesBookIntail extends FeaturesBookState {}
+
+class FeaturesBookLoading extends FeaturesBookState {}
+
+class FeaturesBookSuccess extends FeaturesBookState {
+  final List<BookModel> books;
+
+  FeaturesBookSuccess({required this.books});
+}
+
+class FeaturesBookFailure extends FeaturesBookState {
+  final String errorMessage;
+
+  FeaturesBookFailure({required this.errorMessage});
+}
